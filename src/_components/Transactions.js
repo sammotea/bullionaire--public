f47b4091472c4df9cbc273a20ceeccc8bb7b159c
Sam Thompson <sam.thompson.work@gmail.com>
2020-07-19 21:55:30 +0100

    [ Refactor ] Convert all class components to functional components, inc Transactions…

--- src/_components/Transactions.js
+++ src/_components/Transactions.js
@@ -1,56 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import * as f from "../_helpers/formatter";
 
 import TransactionNavigation from "../_components/TransactionNavigation";
 import TransactionSummary from "../_components/TransactionSummary";
 import TransactionList from "../_components/TransactionList";
 
-class Transactions extends React.Component {
-  constructor(props) {
-    super(props);
+function Transactions(props) {
+  const [filters, setFilters] = useState({
+    showAssets: "all",
+    showActions: "all",
+    showPeriods: "all",
+  });
 
-    this.state = {
-      showAssets: "all",
-      showActions: "all",
-      showPeriods: "all",
-    };
+  const transactionParser = props.transactionParser;
+
+  function handleUserSelection(e) {
+    let tempObj = {};
+    tempObj[f.showify(e.target.id)] = e.target.value;
+
+    const updatedFilters = Object.assign({}, filters, tempObj);
+
+    setFilters(updatedFilters);
   }
 
-  handleUserSelection = (e) => {
-    let newState = {};
+  return (
+    <>
+      <h1>Transactions</h1>
 
-    newState[f.showify(e.target.id)] = e.target.value;
+      <TransactionNavigation
+        assets={transactionParser.getAssetTypes()}
+        periods={transactionParser.getTransactionPeriods()}
+        selectionHandler={handleUserSelection}
+        {...filters}
+      />
 
-    this.setState(newState);
-  };
+      <TransactionSummary
+        transactionsByYear={transactionParser.getTransactionsByYear()}
+        transactionsByAsset={transactionParser.getTransactionsByAsset()}
+        {...filters}
+      />
 
-  render() {
-    const transactionParser = this.props.transactionParser;
-
-    return (
-      <>
-        <h1>Transactions</h1>
-
-        <TransactionNavigation
-          assets={transactionParser.getAssetTypes()}
-          periods={transactionParser.getTransactionPeriods()}
-          selectionHandler={this.handleUserSelection}
-          {...this.state}
-        />
-
-        <TransactionSummary
-          transactionsByYear={transactionParser.getTransactionsByYear()}
-          transactionsByAsset={transactionParser.getTransactionsByAsset()}
-          {...this.state}
-        />
-
-        <TransactionList
-          transactionsByYear={transactionParser.getTransactionsByYear()}
-          {...this.state}
-        />
-      </>
-    );
-  }
+      <TransactionList
+        transactionsByYear={transactionParser.getTransactionsByYear()}
+        {...filters}
+      />
+    </>
+  );
 }
 
 export default Transactions;
