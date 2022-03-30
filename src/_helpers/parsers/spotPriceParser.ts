70dfb9da891fdd8d391bcec6d2ad0ab2dd13d5c5
Sam Thompson <sam.thompson.work@gmail.com>
2022-03-30 12:28:39 +0100

    [ TR ][ Refactor ] Remove unnecessary type definitions.

--- src/_helpers/parsers/spotPriceParser.ts
+++ src/_helpers/parsers/spotPriceParser.ts
@@ -1,64 +1,59 @@
-type BullionTypes = "gold" | "silver";
-type SpotPrices = {
-  [K in BullionTypes]: number;
-};
-
 interface ISpotPriceObject {
-  rates: {
-    [propName: string]: number;
-  };
-  unit: string;
+   rates: {
+      [propName: string]: number;
+   };
+   unit: string;
 }
 
 export const transformSpotPriceObject = (
-  spotPriceObject: ISpotPriceObject
+   spotPriceObject: ISpotPriceObject
 ) => {
-  let spo = { ...spotPriceObject };
+   let spo = { ...spotPriceObject };
 
-  if (spo && spo.rates) {
-    let transformedSpotPrices: { [propName: string]: number } = {};
+   if (spo && spo.rates) {
+      let transformedSpotPrices: { [propName: string]: number } = {};
 
-    for (const type in spo.rates) {
-      transformedSpotPrices[englishifySpotPrices(type)] = kiloify(
-        spo.rates[type],
-        spo.unit
-      );
-    }
+      for (const type in spo.rates) {
+         transformedSpotPrices[englishifySpotPrices(type)] = kiloify(
+            spo.rates[type],
+            spo.unit
+         );
+      }
 
-    return transformedSpotPrices as SpotPrices;
-  } else {
-    return false;
-  }
+      return transformedSpotPrices as SpotPrices;
+   } else {
+      return false;
+   }
 };
 
 export const kiloify = (amount: number, unit: string) => {
-  switch (unit) {
-    case "per ounce":
-      amount = amount * 32.151;
-      break;
+   switch (unit) {
+      case "per ounce":
+         amount = amount * 32.151;
+         break;
 
-    default:
-      // PENDING Throw error and fallback action
-      console.log("ERROR!");
-      amount = 0;
-  }
+      default:
+         // PENDING Throw error and fallback action
+         console.log("ERROR!");
+         amount = 0;
+   }
 
-  return amount;
+   return amount;
 };
 
 export const englishifySpotPrices = (name: string) => {
-  switch (name) {
-    case "XAG":
-      name = "silver";
-      break;
+   switch (name) {
+      case "XAG":
+         name = "silver";
+         break;
 
-    case "XAU":
-      name = "gold";
-      break;
+      case "XAU":
+         name = "gold";
+         break;
 
-    default:
-      break;
-  }
+      default:
+         break;
+   }
 
-  return name;
+   return name;
 };
